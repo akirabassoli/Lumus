799a278ca8f6a6eb33031e59ea2d85391d521b75
Akira Bassoli <akirabassoli@gmail.com>
2023-09-14 19:53:45 -0300

    final

---
+++ js/api.js
@@ -0,0 +1,42 @@
+
+let charactersList = document.getElementById('charactersList');
+let hpCharacters = [];
+
+
+
+let loadCharacters = async () => {
+    try {
+        let res = await fetch('https://hp-api.onrender.com/api/characters');
+        hpCharacters = await res.json();
+        displayCharacters(hpCharacters);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+let displayCharacters = (characters) => {
+    let htmlString = characters
+        .map((character) => {
+            return `
+            <li class="character">
+                <h2>${character.name}</h2>
+                <p>House: ${character.house}</p>
+                <br>
+                <h4>Actor: ${character.actor}</h4>
+                <img src="${character.image}"></img>
+            </li>
+        `;
+        })
+        .join('');
+    charactersList.innerHTML = htmlString;
+};
+
+let filterCharacters = () => {
+    const bar = document.getElementById('searchBar');
+    console.log(hpCharacters)
+    const filtered = hpCharacters.filter(character => character.name.includes(bar.value));
+    displayCharacters(filtered);
+}
+
+
+loadCharacters();
